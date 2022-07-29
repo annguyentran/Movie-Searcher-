bbd1597226469b0bfb8270302dd2ec7e57120ee1
Andrew Nguyen-Tran <ann013@ucsd.edu>
2022-07-28 18:47:15 -0700

    anthonyHelpedUS

--- logic.js
+++ logic.js
@@ -1,5 +1,30 @@
 const movieDescription = document.getElementById("movieDetails")
 const movieImage = document.getElementById('movieImage')
-const searchButtonText = document.getElementById('searchButton')
+const searchButton = document.getElementById('searchButton')
 
-fetch()
+var movieTitle = document.getElementById("searchButton")
+var movieInputEl = document.getElementById("movie")
+var apiKey = "50df99c2"
+
+function getMovie() {
+var userInput = movieInputEl.value.toLowerCase().trim()
+const spacesReplaced = userInput.replace(/ /g, '+');
+
+console.log(spacesReplaced)
+
+var apiURL = "http://www.omdbapi.com/?t="+userInput+"&apikey="+apiKey
+
+fetch(apiURL)
+.then(function(response){
+    console.log(response)
+    return response.json()
+})
+.then(function(data){console.log(data)})
+}
+
+searchButton.addEventListener('click', function(event) {
+event.preventDefault()
+getMovie()
+}) 
+
+
